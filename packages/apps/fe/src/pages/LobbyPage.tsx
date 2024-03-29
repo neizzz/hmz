59071ebf2940d44eedcaccb743d1185abf9db723
neiz-choi <neiz.choi@kakaocorp.com>
2024-03-29 11:49:37 +0900

    chore: make useless code rightnow to comment

--- packages/apps/fe/src/pages/LobbyPage.tsx
+++ packages/apps/fe/src/pages/LobbyPage.tsx
@@ -1,24 +1,24 @@
-import { useCallback } from 'react';
-import { useHmzClient } from '@hooks/useHmzClient';
-import { useModalController } from '@hooks/useModalController';
-import { useNavigate } from 'react-router-dom';
-import MakeRoomForm from '@components/MakeRoomForm';
+import { useCallback } from "react";
+import { useHmzClient } from "@hooks/useHmzClient";
+// import { useModalController } from '@hooks/useModalController';
+import { useNavigate } from "react-router-dom";
+// import MakeRoomForm from '@components/MakeRoomForm';
 
 const LobbyPage = () => {
   const navigate = useNavigate();
   const client = useHmzClient();
 
-  const makeRoomModalController = useModalController({
-    children: <MakeRoomForm />,
-    onSubmit: () => {
-      makeRoomModalController.hide();
-    },
-  });
+  // const makeRoomModalController = useModalController({
+  //   children: <MakeRoomForm />,
+  //   onSubmit: () => {
+  //     makeRoomModalController.hide();
+  //   },
+  // });
 
   return (
     <div>
-      <button onClick={() => makeRoomModalController.show()}>Make room</button>
-      <makeRoomModalController.AutoModal />
+      {/* <button onClick={() => makeRoomModalController.show()}>Make room</button>
+      <makeRoomModalController.AutoModal /> */}
     </div>
   );
 };
