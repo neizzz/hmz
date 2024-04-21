3226637d3dd735917686d777721c6c4c137ec673
ChoongHo <ch.neizzz@gmail.com>
2024-04-21 16:43:17 +0900

    feat: add GameStateQueue

---
+++ packages/apps/fe/src/utils/GameStateQueue.ts
@@ -0,0 +1,38 @@
+import { GameRoomState } from '@schema';
+
+type ConstructorParams<T> = {
+  onLerp?: (a: T, b: T) => T;
+};
+
+export default class GameStateQueue<T = GameRoomState> {
+  private statesFromServer: T[] = []; // 게임 진행중에는 항상 2개를 유지
+  private statesForRender: T[] = []; //
+
+  private onLerp?: (a: T, b: T) => T;
+
+  constructor({ onLerp }: ConstructorParams<T>) {
+    this.onLerp = onLerp;
+  }
+
+  // target: 30hz
+  pushFromServer(state: T) {
+    this.statesFromServer.push(state); // NOTE: 이때 잠깐 3개될 수 있음.
+
+    if (this.statesFromServer.length < 3) return;
+
+    const firstState = this.statesFromServer.shift(); // length: 3 → 2;
+    const secondState = this.statesFromServer[0];
+    this.statesForRender.push(firstState);
+    this.onLerp &&
+      this.statesForRender.push(this.onLerp(firstState, secondState));
+  }
+
+  // target: 60hz
+  popForRender(): T | undefined {
+    if (this.statesForRender.length > 4) {
+      this.statesForRender.length = 4;
+    }
+
+    return this.statesForRender.shift();
+  }
+}
