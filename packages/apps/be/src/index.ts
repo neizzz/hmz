cf9015b40a7a7c2ea645bdd650f17bae2bdbee9c
ChoongHo <ch.neizzz@gmail.com>
2024-06-09 15:14:45 +0900

    refactor: app/be listen with PORT env

--- packages/apps/be/src/index.ts
+++ packages/apps/be/src/index.ts
@@ -11,7 +11,8 @@
 import { listen } from '@colyseus/tools';
 
 // Import Colyseus config
-import app from './app.config.ts';
+import app from './app.config.js';
 
 // Create and listen on 2567 (or PORT environment variable.)
-listen(app);
+const { PORT } = process.env;
+listen(app, PORT ? +PORT : undefined);
