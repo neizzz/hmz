2f818d6b2191afd31ef90d90ab75f7d47c6f2740
ChoongHo <ch.neizzz@gmail.com>
2024-06-13 21:38:45 +0900

    refactor: minify off when development build of be

--- packages/apps/be/vite.config.ts
+++ packages/apps/be/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import { node } from '@liuli-util/vite-plugin-node';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [tsconfigPaths(), node()],
+export default defineConfig(({ command, mode, isPreview }) => {
+  console.log(`Vite mode: ${mode}`);
+  const isDev = mode === 'development';
+  return {
+    build: {
+      sourcemap: isDev,
+      minify: !isDev,
+    },
+    plugins: [tsconfigPaths(), node()],
+  };
 });
