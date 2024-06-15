2c198d0469ed432cfca09272555e1a547dfd9f92
ChoongHo <ch.neizzz@gmail.com>
2024-06-16 01:10:38 +0900

    refactor: remove needless files

--- packages/apps/be/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import { node } from '@liuli-util/vite-plugin-node';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-  plugins: [tsconfigPaths(), node()],
-});
